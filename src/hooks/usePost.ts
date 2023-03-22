c9e13cddbac34a2dbddbeb6cb82bfb7ccf08116f
wojtek99955 <wojtek99955@gmail.com>
2023-03-22 19:15:03 +0100

    update data format

--- src/hooks/usePost.ts
+++ src/hooks/usePost.ts
@@ -12,7 +12,7 @@ export const usePost = () => {
     const requestOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ formData }),
+      body: JSON.stringify(formData),
     };
 
     try {
